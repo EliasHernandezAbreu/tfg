e2559dfdd7fdd1e1b2381494b10c0e0cd6291e9d
EliasHernandezAbreu <alu0101487137@ull.edu.es>
2025-04-28 22:46:01 +0100

    replacement prototype

--- lib/replacement/replacement_page.dart
+++ lib/replacement/replacement_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:tfg/replacement/algorithms/replacement_fifo.dart';
+import 'package:tfg/replacement/replacement_algorithm.dart';
+import 'package:tfg/replacement/replacement_context.dart';
 
 class ReplacementPage extends StatefulWidget {
   const ReplacementPage({super.key});
@@ -10,15 +13,84 @@ class ReplacementPage extends StatefulWidget {
 }
 
 class _ReplacementPageState extends State<ReplacementPage> {
-  int pageFrames = 1;
+  int _pageFrames = 1;
+  final ReplacementAlgorithm _algorithm = ReplacementFifo();
+  ReplacementContext _context = ReplacementContext(4);
+
+  void nextStep() {
+    setState(() {
+      _context = _algorithm.nextState(_context, 0);
+    });
+  }
+  
+  void startSimulation() {
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.all(8),
+    return Padding(
+      padding: const EdgeInsets.all(8),
       child: Column(
         children: [
-          Text("something something")
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Text(
+                "Planned",
+                style: TextStyle(
+                  fontSize: 36,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              TextFormField(),
+              Row(children: [
+                ElevatedButton.icon(
+                  onPressed: startSimulation,
+                  label: const Text("yea")),
+                const DropdownMenu(
+                  initialSelection: 0,
+                  dropdownMenuEntries: [
+                    DropdownMenuEntry(value: 0, label: "fifo"),
+                    DropdownMenuEntry(value: 1, label: "optimal"),
+                    DropdownMenuEntry(value: 2, label: "lru"),
+                    DropdownMenuEntry(value: 3, label: "mru"),
+                  ]
+                )
+
+              ])
+            ],
+          ),
+          Container(
+            color: Colors.blue,
+            height: 2,
+            margin: const EdgeInsets.symmetric(vertical: 24),
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Text(
+                "Interactive",
+                style: TextStyle(
+                  fontSize: 36,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Row(children: [
+                ElevatedButton.icon(
+                  onPressed: startSimulation,
+                  label: const Text("yea")),
+                const DropdownMenu(
+                  initialSelection: 0,
+                  dropdownMenuEntries: [
+                    DropdownMenuEntry(value: 0, label: "fifo"),
+                    DropdownMenuEntry(value: 2, label: "lru"),
+                    DropdownMenuEntry(value: 3, label: "mru"),
+                  ]
+                )
+
+              ])
+            ],
+          ),
         ],
       ),
     );
